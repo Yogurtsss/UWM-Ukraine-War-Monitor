92ab43cdff886634638ccfd17d2eef785881dbaf
Your Name <ledstart@gmail.com>
2026-04-06 20:02:01 +0300

    UWM Cloud Deploy: Full Frontend + Relay @ 06/04/2026 20:02:01.31

---
+++ src/frontend/.next/static/chunks/app/layout-bb648ab5e1b04a43.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6513:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,1371,23)),Promise.resolve().then(l.t.bind(l,6754,23))},6754:()=>{},1371:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f367f3"}}},e=>{var s=s=>e(e.s=s);e.O(0,[364,130,182,744],()=>s(6513)),_N_E=e.O()}]);
