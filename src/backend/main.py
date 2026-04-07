4bda64f4b3589cf2971447bfb9c6fa96ef1532c5
Your Name <ledstart@gmail.com>
2026-04-07 13:57:47 +0300

    UWM Cloud Deploy: Full Frontend + Relay @ 07/04/2026 13:57:47.79

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Header, HTTPException, status
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Header, HTTPException, status, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, JSONResponse
